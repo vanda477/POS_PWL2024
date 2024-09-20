d5478d365928bc7c9eec58ef162883a4f3fdee70
Sheva Ananda Arifiansyah <arifiansyahshevaananda@gmail.com>
2024-09-20 08:22:11 +0700

    jobsheet 4_prak-1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,9 +21,14 @@ class UserController extends Controller
         // UserModel::insert($data);
 
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+
+
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
