95d231ab2cd7099ba8d426067726167996123fbc
Sheva Ananda Arifiansyah <arifiansyahshevaananda@gmail.com>
2024-09-20 09:44:23 +0700

    jobsheet 4 praktikum-2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,9 +37,12 @@ class UserController extends Controller
         //     abort(404);
         // });
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
 
         return view('user', ['data' => $user]);
     }
